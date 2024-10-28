10d4343d0bd817d0456c06b8048e65dc1c5e4a6e
kasyan1337 <tristokorun@gmail.com>
2024-10-28 15:42:02 +0100

    final changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.bulk_mail import send_bulk_emails
 
 if __name__ == "__main__":
     # Configuration
-    email_account_name = "WTM"  # Name of the account as defined in .env
+    email_account_name = "EMAIL1"  # Name of the account as defined in .env
     recipients_filename = "recipients.txt"  # Recipients file in the data folder
     subject_filename = "subject.txt"  # Subject file in the data folder
     body_filename = "body.html"  # Email body file (body.txt or body.html) in the data folder
